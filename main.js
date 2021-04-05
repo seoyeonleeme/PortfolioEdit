8340abd36070e7034d283e67b9a27b8a119a26e9
Seoyeon Lee <67448999+seoyeonleeme@users.noreply.github.com>
2021-04-05 16:09:06 +0900

    Final touch6

--- main.js
+++ main.js
@@ -5,8 +5,8 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  //console.log(window.scrollY);
-  //console.log(`navbarHeight : ${navbarHeight}`);
+  console.log(window.scrollY);
+  console.log(`navbarHeight : ${navbarHeight}`);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
